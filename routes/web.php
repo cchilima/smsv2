2e94541b33283a0cf3815147c562f175d70ac95e
lavum <lmweemba@zut.edu.zm>
2023-12-15 11:36:58 +0200

    added return redirect()->route('login');

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    \route('login');
+    return redirect()->route('login');
 });
 
 Auth::routes(['register' => false]);
