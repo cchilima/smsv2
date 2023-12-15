99d49cb07e3e0b65e62e47a815e9a70cc75144c0
Stephen Tembo <stephen187dev@gmail.com>
2023-12-15 16:09:39 +0200

    Folder structure enforcement

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,17 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Home\HomeController;
+use App\Http\Controllers\Academics\CourseController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return redirect()->route('login');
-});
+
+Route::get('/', function () { return redirect()->route('login'); });
 
 Auth::routes(['register' => false]);
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('courses', CoursesController::class);
+
 
