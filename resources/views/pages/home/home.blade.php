a7a967e40ab7366c1cd36a97fb4c6950ab6d9ad0
lmweemba <lmweemba@zut.edu.zm>
2024-03-25 09:41:26 +0200

    enrollments reports update

--- resources/views/pages/home/home.blade.php
+++ resources/views/pages/home/home.blade.php
@@ -13,11 +13,11 @@
                     <div class="media">
                         <div class="media-body">
                             <h3 class="mb-0">{{ 00 }}</h3>
-                            <span class="text-uppercase font-size-xs font-weight-bold">Total Students</span>
+                            <span class="text-uppercase font-size-xs font-weight-bold">Today's Payments</span>
                         </div>
 
                         <div class="ml-3 align-self-center">
-                            <i class="icon-users4 icon-3x opacity-75"></i>
+                            <i class="icon-coin-dollar icon-3x opacity-75"></i>
                         </div>
                     </div>
                 </div>
@@ -28,7 +28,69 @@
                     <div class="media">
                         <div class="media-body">
                             <h3 class="mb-0">{{ 00 }}</h3>
-                            <span class="text-uppercase font-size-xs">Total Staff</span>
+                            <span class="text-uppercase font-size-xs">Today's invoices</span>
+                        </div>
+
+                        <div class="ml-3 align-self-center">
+                            <i class="icon-credit-card icon-3x opacity-75"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-xl-3">
+                <div class="card card-body bg-success-400 has-bg-image">
+                    <div class="media">
+                        <div class="mr-3 align-self-center">
+                            <i class="icon-people icon-3x opacity-75"></i>
+                        </div>
+
+                        <div class="media-body text-right">
+                            <h3 class="mb-0">{{ 00 }}</h3>
+                            <span class="text-uppercase font-size-xs">Active students</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-xl-3">
+                <div class="card card-body bg-indigo-400 has-bg-image">
+                    <div class="media">
+                        <div class="mr-3 align-self-center">
+                            <i class="icon-user icon-3x opacity-75"></i>
+                        </div>
+
+                        <div class="media-body text-right">
+                            <h3 class="mb-0">{{ 1 }}</h3>
+                            <span class="text-uppercase font-size-xs">Registered Students</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-6 col-xl-3">
+                <div class="card card-body bg-dark has-bg-image">
+                    <div class="media">
+                        <div class="media-body">
+                            <h3 class="mb-0">{{ 00 }}</h3>
+                            <span class="text-uppercase font-size-xs font-weight-bold">Today's Applicants</span>
+                        </div>
+
+                        <div class="ml-3 align-self-center">
+                            <i class="icon-users4 icon-3x opacity-75"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-xl-3">
+                <div class="card card-body bg-orange has-bg-image">
+                    <div class="media">
+                        <div class="media-body">
+                            <h3 class="mb-0">{{ 00 }}</h3>
+                            <span class="text-uppercase font-size-xs">All Applicants</span>
                         </div>
 
                         <div class="ml-3 align-self-center">
@@ -39,7 +101,7 @@
             </div>
 
             <div class="col-sm-6 col-xl-3">
-                <div class="card card-body bg-success-400 has-bg-image">
+                <div class="card card-body bg-blue has-bg-image">
                     <div class="media">
                         <div class="mr-3 align-self-center">
                             <i class="icon-pointer icon-3x opacity-75"></i>
@@ -54,7 +116,69 @@
             </div>
 
             <div class="col-sm-6 col-xl-3">
-                <div class="card card-body bg-indigo-400 has-bg-image">
+                <div class="card card-body bg-indigo-800 has-bg-image">
+                    <div class="media">
+                        <div class="mr-3 align-self-center">
+                            <i class="icon-user icon-3x opacity-75"></i>
+                        </div>
+
+                        <div class="media-body text-right">
+                            <h3 class="mb-0">{{ 1 }}</h3>
+                            <span class="text-uppercase font-size-xs">Total Users</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-6 col-xl-3">
+                <div class="card card-body bg-grey has-bg-image">
+                    <div class="media">
+                        <div class="media-body">
+                            <h3 class="mb-0">{{ $students }}</h3>
+                            <span class="text-uppercase font-size-xs font-weight-bold">Total Students</span>
+                        </div>
+
+                        <div class="ml-3 align-self-center">
+                            <i class="icon-users4 icon-3x opacity-75"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-xl-3">
+                <div class="card card-body bg-violet has-bg-image">
+                    <div class="media">
+                        <div class="media-body">
+                            <h3 class="mb-0">{{ 00 }}</h3>
+                            <span class="text-uppercase font-size-xs">Total Staff</span>
+                        </div>
+
+                        <div class="ml-3 align-self-center">
+                            <i class="icon-users2 icon-3x opacity-75"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-xl-3">
+                <div class="card card-body bg-teal has-bg-image">
+                    <div class="media">
+                        <div class="mr-3 align-self-center">
+                            <i class="icon-pointer icon-3x opacity-75"></i>
+                        </div>
+
+                        <div class="media-body text-right">
+                            <h3 class="mb-0">{{ 00 }}</h3>
+                            <span class="text-uppercase font-size-xs">Total Administrators</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-xl-3">
+                <div class="card card-body bg-brown has-bg-image">
                     <div class="media">
                         <div class="mr-3 align-self-center">
                             <i class="icon-user icon-3x opacity-75"></i>
