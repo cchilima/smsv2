65fac17c6029f5689e09ed1026a4b19ad58269c0
Blessed Zulu <43051370+blessedzulu@users.noreply.github.com>
2024-08-06 16:14:35 +0200

    Renamed prerequisite data table export file name

--- app/Livewire/DataTables/Academics/Prerequisites.php
+++ app/Livewire/DataTables/Academics/Prerequisites.php
@@ -26,7 +26,7 @@ final class Prerequisites extends PowerGridComponent
         $this->showCheckBox();
 
         return [
-            Exportable::make('export')
+            Exportable::make('prerequisites-export')
                 ->striped()
                 ->type(Exportable::TYPE_XLS, Exportable::TYPE_CSV),
             Header::make()->showSearchInput(),
