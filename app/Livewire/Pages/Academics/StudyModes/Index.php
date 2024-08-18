b41892851d83205b31d7a6bfff61825628595efc
Blessed Zulu <43051370+blessedzulu@users.noreply.github.com>
2024-08-18 12:21:46 +0200

    Added reactivity to study-modes.index page

--- app/Livewire/Pages/Academics/StudyModes/Index.php
+++ app/Livewire/Pages/Academics/StudyModes/Index.php
@@ -2,10 +2,15 @@
 
 namespace App\Livewire\Pages\Academics\StudyModes;
 
+use App\Traits\CanRefreshDataTable;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+    use CanRefreshDataTable;
+
+    #[Layout('components.layouts.app-bootstrap')]
     public function render()
     {
         return view('livewire.pages.academics.study-modes.index');
