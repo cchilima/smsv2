fc745f128635f7e4dcbd61ef12e3eb09ce166c2e
Blessed Zulu <43051370+blessedzulu@users.noreply.github.com>
2024-08-18 12:35:59 +0200

    Added reactivity to period-types.index page

--- app/Livewire/Pages/Academics/AcademicPeriodTypes/Index.php
+++ app/Livewire/Pages/Academics/AcademicPeriodTypes/Index.php
@@ -2,10 +2,15 @@
 
 namespace App\Livewire\Pages\Academics\AcademicPeriodTypes;
 
+use App\Traits\CanRefreshDataTable;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+    use CanRefreshDataTable;
+
+    #[Layout('components.layouts.app-bootstrap')]
     public function render()
     {
         return view('livewire.pages.academics.academic-period-types.index');
--- resources/views/livewire/pages/academics/academic-period-types/index.blade.php
+++ resources/views/livewire/pages/academics/academic-period-types/index.blade.php
@@ -1,3 +1,56 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+@section('page_title', 'Manage Academic Period types')
+
+<div class="card">
+    <div class="card-header header-elements-inline">
+        <h6 class="card-title">Manage Academic Period Types</h6>
+        {!! 00 !!}
+    </div>
+
+    <div class="card-body">
+        <ul wire:ignore class="nav nav-tabs nav-tabs-highlight">
+            <li class="nav-item"><a href="#all-types" class="nav-link active" data-toggle="tab">Manage Academic Period
+                    types</a></li>
+            <li class="nav-item"><a href="#new-type" class="nav-link" data-toggle="tab"><i class="icon-plus2"></i> Create
+                    Academic Period type</a></li>
+        </ul>
+
+        <div class="tab-content">
+            <div wire:ignore.self class="tab-pane fade show active" id="all-types">
+                <livewire:datatables.academics.academic-period-types />
+            </div>
+
+            <div wire:ignore.self class="tab-pane fade" id="new-type">
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <form class="ajax-store" method="post" action="{{ route('period-types.store') }}">
+                            @csrf
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label font-weight-semibold">Name <span
+                                        class="text-danger">*</span></label>
+                                <div class="col-lg-9">
+                                    <input name="name" value="{{ old('name') }}" required type="text"
+                                        class="form-control" placeholder="Name of Academic Period type">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label font-weight-semibold">Description</label>
+                                <div class="col-lg-9">
+                                    <input name="description" value="{{ old('description') }}" type="text"
+                                        class="form-control" placeholder="Description of Academic Period type">
+                                </div>
+                            </div>
+
+                            <div class="text-right">
+                                <button wire:click="refreshTable('AcademicPeriodTypesTable')" id="ajax-btn"
+                                    type="submit" class="btn btn-primary">Submit form <i
+                                        class="icon-paperplane ml-2"></i></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
