10e8cac65da347567f5f98104c57336b0add07a3
Blessed Zulu <43051370+blessedzulu@users.noreply.github.com>
2024-08-19 16:25:12 +0200

    Added reactivity to provinces.index page

--- app/Livewire/Pages/Residency/Provinces/Index.php
+++ app/Livewire/Pages/Residency/Provinces/Index.php
@@ -2,12 +2,34 @@
 
 namespace App\Livewire\Pages\Residency\Provinces;
 
+use App\Helpers\Qs;
+use App\Repositories\Residency\CountryRepository;
+use App\Traits\CanRefreshDataTable;
+use Illuminate\Support\Facades\Gate;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+    use CanRefreshDataTable;
+
+    protected CountryRepository $countryRepo;
+
+    public function boot()
+    {
+        $this->countryRepo = new CountryRepository();
+    }
+
+    public function mount()
+    {
+        Gate::allowIf(Qs::userIsTeamSA() || Qs::userIsSuperAdmin());
+    }
+
+    #[Layout('components.layouts.app-bootstrap')]
     public function render()
     {
-        return view('livewire.pages.residency.provinces.index');
+        return view('livewire.pages.residency.provinces.index', [
+            'countries' => $this->countryRepo->getAll(),
+        ]);
     }
 }
--- resources/views/livewire/pages/residency/provinces/index.blade.php
+++ resources/views/livewire/pages/residency/provinces/index.blade.php
@@ -1,3 +1,65 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+@section('page_title', 'Manage Provinces')
+
+@php
+    use App\Helpers\Qs;
+@endphp
+
+<div class="card">
+    <div class="card-header header-elements-inline">
+        <h6 class="card-title">Manage Provinces</h6>
+        {!! Qs::getPanelOptions() !!}
+    </div>
+
+    <div class="card-body">
+        <ul wire:ignore class="nav nav-tabs nav-tabs-highlight">
+            <li class="nav-item"><a href="#all-provinces" class="nav-link active" data-toggle="tab">Manage Provinces</a>
+            </li>
+            <li class="nav-item"><a href="#new-province" class="nav-link" data-toggle="tab"><i class="icon-plus2"></i>
+                    Create New Province</a></li>
+        </ul>
+
+        <div class="tab-content">
+            <div wire:ignore.self class="tab-pane fade show active" id="all-provinces">
+                <livewire:datatables.residency.provinces />
+            </div>
+
+            <div wire:ignore class="tab-pane fade" id="new-province">
+                <div class="row">
+                    <div class="col-md-6">
+                        <form class="ajax-store" method="post" action="{{ route('provinces.store') }}">
+                            @csrf
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label font-weight-semibold">Province <span
+                                        class="text-danger">*</span></label>
+                                <div class="col-lg-9">
+                                    <input name="name" value="{{ old('name') }}" required type="text"
+                                        class="form-control" placeholder="Copperbelt">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label font-weight-semibold">Country: <span
+                                        class="text-danger">*</span></label>
+                                <div class="col-lg-9">
+                                    <select data-placeholder="Select Country" required
+                                        class="select-search form-control" name="country_id" id="country_id">
+                                        <option disabled selected value=""></option>
+                                        @foreach ($countries as $country)
+                                            <option value="{{ $country->id }}">{{ $country->country }}</option>
+                                        @endforeach
+                                    </select>
+
+                                </div>
+                            </div>
+
+                            <div class="text-right">
+                                <button wire:click="refreshTable('ProvincesTable')" id="ajax-btn" type="submit"
+                                    class="btn btn-primary">Submit form <i class="icon-paperplane ml-2"></i></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
