3740f77d695a0cf201bd9b9ea4e71e7eff533b66
Blessed Zulu <43051370+blessedzulu@users.noreply.github.com>
2024-08-19 08:13:24 +0200

    Added reactitivy to intakes.index page

--- app/Livewire/Pages/Academics/Intakes/Index.php
+++ app/Livewire/Pages/Academics/Intakes/Index.php
@@ -2,10 +2,15 @@
 
 namespace App\Livewire\Pages\Academics\Intakes;
 
+use App\Traits\CanRefreshDataTable;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+    use CanRefreshDataTable;
+
+    #[Layout('components.layouts.app-bootstrap')]
     public function render()
     {
         return view('livewire.pages.academics.intakes.index');
--- resources/views/livewire/pages/academics/intakes/index.blade.php
+++ resources/views/livewire/pages/academics/intakes/index.blade.php
@@ -1,3 +1,50 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+@section('page_title', 'Intakes')
+
+@php
+    use App\Helpers\Qs;
+@endphp
+
+<div class="card">
+    <div class="card-header header-elements-inline">
+        <h6 class="card-title">Manage Intakes</h6>
+        {!! Qs::getPanelOptions() !!}
+    </div>
+
+    <div class="card-body">
+        <ul wire:ignore class="nav nav-tabs nav-tabs-highlight">
+            <li class="nav-item"><a href="#all-classes" class="nav-link active" data-toggle="tab">Manage Intakes</a></li>
+            <li class="nav-item"><a href="#new-class" class="nav-link" data-toggle="tab"><i class="icon-plus2"></i>
+                    Create New Intake</a></li>
+        </ul>
+
+        <div class="tab-content">
+            <div wire:ignore.self class="tab-pane fade show active" id="all-classes">
+                <livewire:datatables.academics.academic-period-intakes />
+            </div>
+
+            <div wire:ignore.self class="tab-pane fade" id="new-class">
+                <div class="row">
+                    <div class="col-md-6">
+                        <form class="ajax-store" method="post" action="{{ route('intakes.store') }}">
+                            @csrf
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label font-weight-semibold">Intake Name <span
+                                        class="text-danger">*</span></label>
+                                <div class="col-lg-9">
+                                    <input name="name" value="{{ old('name') }}" required type="text"
+                                        class="form-control" placeholder="Intakes">
+                                </div>
+                            </div>
+
+                            <div class="text-right">
+                                <button wire:click="refreshTable('AcademicPeriodIntakesTable')" id="ajax-btn"
+                                    type="submit" class="btn btn-primary">Submit form <i
+                                        class="icon-paperplane ml-2"></i></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
