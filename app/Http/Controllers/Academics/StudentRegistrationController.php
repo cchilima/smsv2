06a801f23a47ae1ec4c3d4c1807091a72d2739f0
Blessed Zulu <43051370+blessedzulu@users.noreply.github.com>
2024-09-10 08:25:38 +0200

    Added error handling to registration summary download functionality

--- app/Http/Controllers/Academics/StudentRegistrationController.php
+++ app/Http/Controllers/Academics/StudentRegistrationController.php
@@ -100,47 +100,50 @@ class StudentRegistrationController extends Controller
      */
     public function summary(Request $request)
     {
+        try {
+            // Incase request from management get student number
+            $studentNumber = $request->input('student_number');
+            $academicPeriodId = $request->input('academic_period_id');
 
-        // Incase request from management get student number
-        $studentNumber = $request->input('student_number');
-        $academicPeriodId = $request->input('academic_period_id');
+            // If request is from student
+            if (!$studentNumber) {
+                $studentNumber = Auth::user()->student->id;
+                $academicPeriodId = Auth::user()->student->academic_info->academic_period->id;
+            }
 
-        // If request is from student
-        if (!$studentNumber) {
-            $studentNumber = Auth::user()->student->id;
-            $academicPeriodId = Auth::user()->student->academic_info->academic_period->id;
+            $courses = $this->registrationRepo->getSummaryCourses($studentNumber, $academicPeriodId);
+            $student = $this->registrationRepo->getStudent($studentNumber);
+            $studentUser = $student->user;
+            $studentUserPersonalInfo = $studentUser->userPersonalInfo;
+            $nextOfKin = $studentUser->userNextOfKin;
+            $academicInfo = $this->registrationRepo->getSummaryAcademicInfo($academicPeriodId);
+            $latestEnrollment = $student->enrollments->sortBy('created_at')->last();
+
+            $fileName = $student->id . '-registration-summary-' . now()->format('d-m-Y-His') . '.pdf';
+
+            $studentInfo = [
+                'Student Name' => $studentUser->first_name . ' ' . $studentUser->last_name,
+                'Student ID' => $student->id,
+                'Gender' => $studentUser->gender,
+                'NRC Number' => $studentUserPersonalInfo->nrc,
+                'Next of Kin' => $nextOfKin->full_name,
+                'Next of Kin Relationship' => $nextOfKin->relationship->relationship,
+                'Next of Kin Contact' => $nextOfKin->mobile,
+            ];
+
+            $admissionInfo = [
+                'Program of Study' => $student->program->name . ' (' . $student->program->code . ')',
+                'Mode of Study' => $student->study_mode->name,
+                'Registration Date' => $latestEnrollment->created_at->format('d F Y H:i'),
+                'Academic Period' => $academicInfo->academic_period->name,
+                'Year of Study' => $student->level->name
+            ];
+
+            $pdf = Pdf::loadView('templates.pdf.registration-summary', compact('studentInfo', 'admissionInfo', 'courses'));
+
+            return $pdf->download($fileName);
+        } catch (\Throwable $th) {
+            return Qs::jsonError('Failed to download registration summary: ' . $th->getMessage());
         }
-
-        $courses = $this->registrationRepo->getSummaryCourses($studentNumber, $academicPeriodId);
-        $student = $this->registrationRepo->getStudent($studentNumber);
-        $studentUser = $student->user;
-        $studentUserPersonalInfo = $studentUser->userPersonalInfo;
-        $nextOfKin = $studentUser->userNextOfKin;
-        $academicInfo = $this->registrationRepo->getSummaryAcademicInfo($academicPeriodId);
-        $latestEnrollment = $student->enrollments->sortBy('created_at')->last();
-
-        $fileName = $student->id . '-registration-summary-' . now()->format('d-m-Y-His') . '.pdf';
-
-        $studentInfo = [
-            'Student Name' => $studentUser->first_name . ' ' . $studentUser->last_name,
-            'Student ID' => $student->id,
-            'Gender' => $studentUser->gender,
-            'NRC Number' => $studentUserPersonalInfo->nrc,
-            'Next of Kin' => $nextOfKin->full_name,
-            'Next of Kin Relationship' => $nextOfKin->relationship->relationship,
-            'Next of Kin Contact' => $nextOfKin->mobile,
-        ];
-
-        $admissionInfo = [
-            'Program of Study' => $student->program->name . ' (' . $student->program->code . ')',
-            'Mode of Study' => $student->study_mode->name,
-            'Registration Date' => $latestEnrollment->created_at->format('d F Y H:i'),
-            'Academic Period' => $academicInfo->academic_period->name,
-            'Year of Study' => $student->level->name
-        ];
-
-        $pdf = Pdf::loadView('templates.pdf.registration-summary', compact('studentInfo', 'admissionInfo', 'courses'));
-
-        return $pdf->download($fileName);
     }
 }
